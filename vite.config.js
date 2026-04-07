6f00bc42d04b919d1af4b49ad61c215d046781e7
zahraa-01 <zahraa.karim@booking.com>
2026-04-07 22:54:39 +0100

    Updated dependencies in package.json

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      },
-    },
-  },
   test: {
     environment: 'jsdom',
     globals: true,
